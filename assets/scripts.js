f7edcd8dfdf5f1b73b97da584d3ea3656dd86773
RuthDanielaAguirre <argdiee@icloud.com>
2024-10-23 14:56:05 +0200

    hotfix titles

--- assets/scripts.js
+++ assets/scripts.js
@@ -45,8 +45,9 @@ document.addEventListener("DOMContentLoaded", function () {
             </div>
                 <div class = "footer-texto">
                 <p>&copy; 2024 GreenDev - Tecnología para un futuro sostenible</p>
-                <p>Dirección: Calle Verde 123, Ciudad Eco, España | Teléfono: +34 123 456 789</p>
-                <p><a href="mailto:info@greendev.com">info@greendev.com</a> | Política de Neutralidad de CO2</p>
+                <p>Dirección: Calle Verde 123, Ciudad Eco, España </p>
+                <p>Teléfono: 123 456 789 | <a href="mailto:info@greendev.com">info@greendev.com</a> </p> 
+                <p>Política de Neutralidad de CO2</p>
                 </div>
             </footer>
         `;
